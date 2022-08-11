93fee11edd4b01ebe56d34a337532a161b87c909
Joao Sanchez <joao.sanchezz@hotmail.com>
2022-08-10 22:27:44 -0300

    Starting to structure how is going to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-
+import { useState, useEffect } from 'react';
+import { QRCode } from './components/QRCode';
+import { Upload } from './components/Upload';
 
 export default function App(){
+
+  const [isLogged, setIsLogged] = useState<boolean>(false)
+
+  useEffect(() => {
+    //fetch
+  })
+
+
+  useEffect(() => {
+    if(!isLogged){
+      setIsLogged(false)
+      return
+    }
+
+    setIsLogged(true)
+
+  },[isLogged])
+
 
   return (
     <div>
       <h1>RPA Reten&ccedil;&atilde;o</h1>
+      {
+        isLogged
+        ? <Upload />
+        : <QRCode />
+      }
     </div>
   )
 }
