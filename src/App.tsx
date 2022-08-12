c4f537e5ed273afdd2ba78f91bb28e8de2de5f39
Joao Sanchez <joao.f13@hotmail.com>
2022-08-11 21:05:37 -0300

    Deciding the best way to fetch the QR code

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,54 @@ import { Upload } from './components/Upload';
 export default function App(){
 
   const [isLogged, setIsLogged] = useState<boolean>(false)
+  const [error, setError] = useState()
+  const [qr_data, setQr_data] = useState("")
+
+  // setar cookie para impedir requisições desnecessarias
+  //1hr de cookie, reqs 10s pro qr
+
+  const handleConnection = (data: any) => {
+    data.connected ? setIsLogged(true) : setIsLogged(false)
+    if(!data.qr_code) return
+    setQr_data(data.qr_code.base64Qr)
+  }
 
   useEffect(() => {
-    //fetch
-  })
 
+    const timer = setInterval(() => {
+      fetch('http://172.16.121.182:22000/status')
+      .then((res) => {
+        if(res.ok) return res.json()
+        throw new Error('Something went wrong')
+      })
+      .then((data) => {
+        handleConnection(data)
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+        setError(err)})
+    }, 5000)
 
-  useEffect(() => {
-    if(!isLogged){
-      setIsLogged(false)
-      return
-    }
-
-    setIsLogged(true)
-
-  },[isLogged])
-
+    return () => clearInterval(timer)
+  },[])
 
   return (
     <div>
       <h1>RPA Reten&ccedil;&atilde;o</h1>
       {
-        isLogged
-        ? <Upload />
-        : <QRCode />
+        error && <h3>Something went wrong</h3>
       }
+      {
+        isLogged && <Upload />
+      }
+      {
+        qr_data && <QRCode qr_data={qr_data} />
+      }
+      <div>
+        <button onClick={() => setIsLogged(true)}>True</button>
+        <button onClick={() => setIsLogged(false)}>False</button>
+      </div>
     </div>
   )
 }
